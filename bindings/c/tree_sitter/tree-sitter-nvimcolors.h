19cb4ef871c766879192c31beb481121c785a104
Louis Chan <louischan0325@gmail.com>
2026-04-10 00:51:27 +0800

    Keep only files necessary for use in Neovim

---
+++ bindings/c/tree_sitter/tree-sitter-nvimcolors.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_NVIMCOLORS_H_
+#define TREE_SITTER_NVIMCOLORS_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_nvimcolors(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_NVIMCOLORS_H_
